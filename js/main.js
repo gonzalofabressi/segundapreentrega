19bf328c30e355f562f05f2b845c9b9d850d47b7
gonzalofabressi <gonzafabressi@hotmail.com.ar>
2023-11-13 19:46:07 -0300

    ultimo guardado con error

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-
+//-----------------------------segunda pre-entrega-------------------------------//
 alert("Bienvenidos a TiendaMultimarca. A continuacion realizaremos una serie de preguntas para ayudarte con la busqueda (no es necesario que contestes todas) ")
 
 
 let marca = prompt("Ingrese la marca que andas buscando").toLowerCase();
 let prenda = prompt("Ingrese la prenda que te interesa").toLowerCase();
-let maximo = Number (prompt("Ingrese el maximo de dinero que quieras gastar en la prenda"))
-let talles = prompt ("Ingrese el talle de la prenda (S, M, L, XL)")
-let color = prompt ("Ingrese el color de la prenda").toLowerCase();
+let maximo = Number(prompt("Ingrese el maximo de dinero que quieras gastar en la prenda"))
+let talles = prompt("Ingrese el talle de la prenda (S, M, L, XL)")
+let color = prompt("Ingrese el color de la prenda").toLowerCase();
 
 
 const tiendas = [
@@ -154,18 +154,18 @@ const tiendas = [
 
 
 function mostrarTiendas(tiendas) {
-    tiendas.forEach((tienda) => console.log(tienda.marca + " - " + tienda.prenda + " - " + tienda.precio + "$ - " + tienda.talles + " - " + tienda.color))
+    tiendas.forEach((tienda) => console.log("Marca: " + tienda.marca + " - Prenda: " + tienda.prenda + " - Precio: " + tienda.precio + "$ - Talles: " + tienda.talles + " - Color: " + tienda.color))
 }
 
 
 function filtrarTiendas() {
-    const resultado = tiendas.filter(filtrarMarca) .filter(filtrarPrenda) .filter(filtrarMaximo) .filter(filtrarTalles) .filter(filtrarColor);
-    if (resultado.length > 0){
+    const resultado = tiendas.filter(filtrarMarca).filter(filtrarPrenda).filter(filtrarMaximo).filter(filtrarTalles).filter(filtrarColor);
+    if (resultado.length > 0) {
         mostrarTiendas(resultado)
-    }else{
-        console.log ( "No tenemos disponible disculpa")
+    } else {
+        console.log("No tenemos disponible disculpa")
     }
-    
+
 }
 
 
@@ -177,14 +177,14 @@ function filtrarMarca(tienda) {
 }
 
 
-function filtrarPrenda (tienda) {
+function filtrarPrenda(tienda) {
     if (prenda) {
         return tienda.prenda.toLowerCase() === prenda
     }
     return tienda;
 }
 
-function filtrarMaximo (tienda) {
+function filtrarMaximo(tienda) {
     if (maximo) {
         return tienda.precio <= maximo
     }
@@ -198,7 +198,7 @@ function filtrarTalles(tienda) {
     return tienda;
 }
 
-function filtrarColor (tienda) {
+function filtrarColor(tienda) {
     if (color) {
         return tienda.color.toLowerCase() === color
     }
@@ -208,4 +208,63 @@ function filtrarColor (tienda) {
 
 filtrarTiendas()
 
-console.log ("Espero hayas encontrado lo que buscabas") 
+
+//-----------------------------------------------------------------//
+//----------------------primer preentrega--------------------------//
+//-----------------------------------------------------------------//
+
+
+
+let total = 0
+
+//funciones//
+
+function valorDonacion(donacion) {
+    total = total + donacion;
+    console.log("Ingresa el valor de " + " " + donacion + " pesos")
+}
+
+function cuotasInteres(cuotas) {
+    return ((total * 0.06 * cuotas) + total) / cuotas;
+}
+
+function cuotasTotal(cuotas) {
+    return (total * 0.06 * cuotas) + total
+}
+
+// proyecto//
+let nombre = prompt("Ingrese su Nombre y Apellido")
+console.log("Bienvenido " + nombre + ". Que desea llevar?")
+
+let ventaProducto = prompt("Ingrese aqui la prenda, marca y talle que desea llevar")
+
+console.log("Usted llevara un/a " + ventaProducto + ". A continuacion ingrese el precio de la prenda por favor. ")
+
+let pesos = Number(prompt("Coloque aqui el valor de la prenda"))
+
+valorDonacion(pesos)
+
+console.log("El unico medio de pago en esta plataforma es con tarjeta de credito, y contamos con hasta 12 cuotas con interes")
+
+let cuotas = Number(prompt("En cuantas cuotas deseas realizar el pago?"))
+
+let montoCuotas = cuotasInteres(cuotas)
+const totalPago = cuotasTotal(cuotas)
+
+if (cuotas == 1) {
+    console.log("Usted va a pagar " + total + "$.")
+} else if (cuotas > 1 && cuotas <= 12) {
+    console.log("Usted va a pagar " + cuotas + " cuotas de " + montoCuotas + " $. " + "Siendo un total de " + totalPago + "$.");
+} else {
+    console.log("Lo sentimos, pero no puedes pagar en tantas cuotas");
+}
+
+
+
+let realizarPago = prompt("¿Desea realizar el pago? (si/no)");
+if (realizarPago.toLowerCase() == "si") {
+    console.log("¡El pago fue realizado con exito! Muchas Gracias");
+} else {
+    console.log("El pago fue cancelado. Gracias de todas formas.");
+}
+
